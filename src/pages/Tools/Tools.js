6ededc6a1cafda8b5142c9c9129e457e21024920
hasansharif819 <hs.sharif819@gmail.com>
2022-06-11 13:51:55 +0600

    smart title

---
+++ src/pages/Tools/Tools.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import Tool from '../Tool/Tool';
+
+const Tools = () => {
+    const [tools, setTools] = useState([]);
+    useEffect(() => {
+        fetch('bhuiyan.json')
+            .then(res => res.json())
+            .then(data => setTools(data))
+    }, [])
+    return (
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 ml-5">
+            {
+                tools.map(tool => <Tool
+                    key={tool.id}
+                    tool={tool}
+                ></Tool>)
+            }
+            <div class="card-actions justify-center mt-10 mb-10">
+            <a href="https://github.com/hasansharif819/warehouse_management_system_client" target="_blank" className="btn btn-primary">Client</a>
+            </div>
+            <div class="card-actions justify-center mt-10 mb-10">
+            <a href="https://smart-zone-819.web.app/" target="_blank" className="btn btn-primary">Live Site</a>
+            </div>
+            <div class="card-actions justify-center mt-10 mb-10">
+            <a href="https://github.com/hasansharif819/warehouse_management_system_server" target="_blank" className="btn btn-primary">Server</a>
+            </div>
+        </div>
+    );
+};
+
+export default Tools;
